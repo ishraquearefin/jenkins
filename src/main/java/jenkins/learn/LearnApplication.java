079203c9c52e4f8bd5b52cee966b0e8141035b52
Arefin <ishraque.arefinrafi@gmail.com>
2025-02-13 00:29:13 +0600

    new one

--- src/main/java/jenkins/learn/LearnApplication.java
+++ src/main/java/jenkins/learn/LearnApplication.java
@@ -1,10 +1,15 @@
 package jenkins.learn;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class LearnApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
